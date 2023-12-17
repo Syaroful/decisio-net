b5f61d4800f7222bb91f0061b56b254b74c39a9b
Syaroful <syaroful10403@gmail.com>
2023-12-17 13:52:06 +0700

    feat: link update on navbar

--- resources/views/dashboard/layouts/side_navbar.blade.php
+++ resources/views/dashboard/layouts/side_navbar.blade.php
@@ -3,7 +3,7 @@
     <div class="h-19.5">
         <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden"
             sidenav-close></i>
-        <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700" href="javascript:;" target="_blank">
+        <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700" href="https://github.com/Syaroful/decisio-net" target="_blank">
             <img src="../assets/img/logo_decisionet.png"
                 class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8" alt="main_logo" />
             <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">Decisio-Net</span>
