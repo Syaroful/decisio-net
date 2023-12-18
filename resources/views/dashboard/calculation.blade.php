d82016cbc4d3636354ea3e96fbdc73ae34d60c5d
Syaroful <syaroful10403@gmail.com>
2023-12-18 08:39:54 +0700

    feat: fix tabel name

--- resources/views/dashboard/calculation.blade.php
+++ resources/views/dashboard/calculation.blade.php
@@ -169,7 +169,7 @@
                 <div
                     class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
                     <div class="p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                        <h6>Solusi Ideal Positif(A+)</h6>
+                        <h6>Solusi Ideal Positif (A+)</h6>
                     </div>
 
                     <div class="flex-auto px-0 pt-0 pb-2">
@@ -223,7 +223,7 @@
                 <div
                     class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
                     <div class="p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                        <h6>Solusi Ideal Negatif(A-)</h6>
+                        <h6>Solusi Ideal Negatif (A-)</h6>
                     </div>
 
                     <div class="flex-auto px-0 pt-0 pb-2">
@@ -277,7 +277,7 @@
                 <div
                     class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
                     <div class="p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                        <h6>Jarak Solusi Ideal Positif (D)</h6>
+                        <h6>Jarak Solusi Ideal Positif dan Negatif (D)</h6>
                     </div>
 
                     <div class="flex-auto px-0 pt-0 pb-2">
@@ -330,7 +330,7 @@
                 <div
                     class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
                     <div class="p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                        <h6>solusi_ideal_negatif</h6>
+                        <h6>Nilai Preverensi (V)</h6>
                     </div>
 
                     <div class="flex-auto px-0 pt-0 pb-2">
@@ -342,6 +342,10 @@
                                             class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                                             Alternative</th>
 
+                                        <th
+                                            class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                            Nama Alternative (nama siswa)
+                                        </th>
                                         <th
                                             class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                                             Nilai Preferensi
@@ -404,7 +408,12 @@
 
                                         <th
                                             class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                            Nilai Preferensi
+                                            Nama Alternative (nama siswa)
+                                        </th>
+
+                                        <th
+                                            class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                            Ranking
                                         </th>
 
                                     </tr>
@@ -428,7 +437,7 @@
                                             <td
                                                 class="p-6 text-start align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
                                                 <span class="text-sm leading-tight text-slate-400">
-                                                    {{ $ranking[$alternative->id-1] ?? '-' }}
+                                                    {{ $ranking[$alternative->id - 1] ?? '-' }}
                                                 </span>
                                             </td>
 
